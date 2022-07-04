1a2b3832f6be0aa30bf9fe1728f94f26b3bf869e
//مجهول// <99824993+AnonymousHm@users.noreply.github.com>
2022-07-04 10:41:08 -0700

    Bot to search stuffs

---
+++ torrentscrape.py
@@ -0,0 +1,25 @@
+from py1337x import py1337x
+
+torrents= py1337x(proxy='1337x.to')
+
+class thirteenX:
+  def __init__(self, query, torrentId):
+    self.query = query
+    self.torrentId = torrentId
+    
+  def search(query):
+    search = torrents.search(query)
+    parser = search.get('items')
+    result = []
+    for i in parser:
+      res = []
+      res.append(i.get('name')) #name
+      res.append(i.get('link')) #link
+      res.append(i.get('size')) # size
+      result.append(res)
+    return result
+      
+  def get_info(torrentId):
+    info = torrents.info(torrentId=torrentId)
+    return [info.get('name'), info.get('category'), info.get('leechers'), info.get('seeders'), info.get('magnetLink'), info.get('size')]
+    
